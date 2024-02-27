2a49dd6092103cfbde6db7d153d8f4f25df6a94a
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-02-27 16:50:59 +0300

    l_2 js change

--- JavaScript/lesson_2/countries.js
+++ JavaScript/lesson_2/countries.js
@@ -1,5 +1,5 @@
-function addCountry(countryName,capitalName) {
-    countrysH[countryName]=capitalName;
+function addCountry(countryName, capitalName) {
+    countrysH[countryName] = capitalName;
 }
 
 function deleteCountry(countryName) {
@@ -7,33 +7,33 @@ function deleteCountry(countryName) {
 }
 
 function getCountryInfo(countryName) {
-    if ( countryName in countrysH )
-        return 'страна: ' + countryName + ' столица: ' + countrysH[countryName] ;
+    if (countryName in countrysH)
+        return 'страна: ' + countryName + ' столица: ' + countrysH[countryName];
     else
-        return 'нет информации о стране ' + countryName + '!' ;
+        return 'нет информации о стране ' + countryName + '!';
 }
 
 function listCountrys() {
-    var res="";
-    for ( var CN in countrysH )
-        res+='\n'+getCountryInfo(CN);
+    var res = "";
+    for (var CN in countrysH)
+        res += '\n' + getCountryInfo(CN);
     return res;
 }
 
 function userAddCountry() {
-    var countryName=prompt('Введите страну: ');
-    var capitalName=prompt('Введите столицу: ');
-    addCountry(countryName,capitalName);
+    var countryName = prompt('Введите страну: ');
+    var capitalName = prompt('Введите столицу: ');
+    addCountry(countryName, capitalName);
     alert(`Страна ${countryName} со столицей в г.${capitalName} добавлена!`);
 }
 
 function userGetCountryInfo() {
-    var countryName=prompt('Введите страну: ');
+    var countryName = prompt('Введите страну: ');
     getCountryInfo(countryName);
-    if ( countryName in countrysH )
+    if (countryName in countrysH)
         alert('страна: ' + countryName + ' столица: ' + countrysH[countryName]);
     else
-        alert('нет информации о стране ' + countryName + '!') ;
+        alert('нет информации о стране ' + countryName + '!');
 }
 
 function userListCountrys() {
@@ -41,7 +41,11 @@ function userListCountrys() {
 }
 
 function userDeleteCountry(countryName) {
-    var countryName=prompt('Введите страну: ');
+    var countryName = prompt('Введите страну: ');
+    if (countryName in countrysH)
+        alert('Cтрана ' + countryName + ' успешно уничтожена!');
+    else
+        alert('Чтобы удалить страну ' + countryName + ', нужно сначала создать такую страну!');
     deleteCountry(countryName);
     console.log("список стран:" + listCountrys());
 }
