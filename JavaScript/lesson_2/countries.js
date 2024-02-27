66aff4664d8e58ec252d21102eeb8b7004b3f09e
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-02-27 15:54:18 +0300

    lesson_2 js

---
+++ JavaScript/lesson_2/countries.js
@@ -0,0 +1,47 @@
+function addCountry(countryName,capitalName) {
+    countrysH[countryName]=capitalName;
+}
+
+function deleteCountry(countryName) {
+    delete countrysH[countryName];
+}
+
+function getCountryInfo(countryName) {
+    if ( countryName in countrysH )
+        return 'страна: ' + countryName + ' столица: ' + countrysH[countryName] ;
+    else
+        return 'нет информации о стране ' + countryName + '!' ;
+}
+
+function listCountrys() {
+    var res="";
+    for ( var CN in countrysH )
+        res+='\n'+getCountryInfo(CN);
+    return res;
+}
+
+function userAddCountry() {
+    var countryName=prompt('Введите страну: ');
+    var capitalName=prompt('Введите столицу: ');
+    addCountry(countryName,capitalName);
+    alert(`Страна ${countryName} со столицей в г.${capitalName} добавлена!`);
+}
+
+function userGetCountryInfo() {
+    var countryName=prompt('Введите страну: ');
+    getCountryInfo(countryName);
+    if ( countryName in countrysH )
+        alert('страна: ' + countryName + ' столица: ' + countrysH[countryName]);
+    else
+        alert('нет информации о стране ' + countryName + '!') ;
+}
+
+function userListCountrys() {
+    console.log("список стран:" + listCountrys());
+}
+
+function userDeleteCountry(countryName) {
+    var countryName=prompt('Введите страну: ');
+    deleteCountry(countryName);
+    console.log("список стран:" + listCountrys());
+}
