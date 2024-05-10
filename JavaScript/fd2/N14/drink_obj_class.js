e076be427d689ba4654d63c3b802741343d29ac7
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-05-10 20:24:00 +0300

    drinkObjClass

---
+++ JavaScript/fd2/N14/drink_obj_class.js
@@ -0,0 +1,25 @@
+class ObjStorageClass {
+    constructor () {
+        this.storage = {}.bind(this);
+    }
+
+    addValue = (key, value) => {
+        this.storage[key] = value;
+    }
+
+    getValue = (key) => {
+        this.storage[key];
+    }
+
+    deleteValue = (key) => {
+        if (key in this.storage) {
+            delete this.storage[key];
+            return true;
+        }
+        return false;
+    }
+
+    getKeys = () => {
+        return Object.keys(this.storage);
+    }
+}
